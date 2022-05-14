737aa5f2ef047b6b2e4de0511281d2c8b435a68a
WarmMilkCodes <mrguybanks@gmail.com>
2022-05-13 23:48:24 -0500

    working on api

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
+from rsa import verify
 import config
+from bs4 import BeautifulSoup
+import json
+import requests
+import html5lib
+
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -13,11 +19,28 @@ def index():
 def submit():
     if request.method == "POST":
         zipCode = request.form['zipCode']
-        print(zipCode)
+        params = {
+        'api_key': 'DDAF9CB2D47F40509CEEE78979E44654',
+        'type': 'search',
+        'search_term': 'baby formula',
+        'category_id': '5427',
+        'customer_zipcode': zipCode
+        }      
+        
+        # make the http GET request to BlueCart API
+        url = requests.get('https://api.bluecartapi.com/request', params)
+        
+        json_result = json.loads(url)
+        print('Store ID: ["store_id"]')            
         return render_template('results.html')
+    
+    
 
+    
+      
+    
 
-
+        
 
 
 
